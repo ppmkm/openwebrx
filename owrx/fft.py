dbbe7d51bba77f8f7a264c1ffd0a410ed1900df5
piotr <piotr@step.misie.local>
2021-08-19 12:02:39 +0200

    averages without overlap set to default of 10 instead of 0.

--- owrx/fft.py
+++ owrx/fft.py
@@ -39,8 +39,8 @@ class SpectrumThread(Output, SdrSourceEventClient):
 
             dsp.set_fft_averages(
                 int(round(1.0 * samp_rate / fft_size / fft_fps / (1.0 - fft_voverlap_factor)))
-                if fft_voverlap_factor > 0
-                else 0
+                if fft_voverlap_factor > 0 
+                else 50
             )
 
         self.subscriptions = [
